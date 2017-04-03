6bae6ac4674c26a37ea9aa5c6d457b06df041b16
digijin <james@digijin.com>
2017-04-03 13:59:27 +1000

    hello world

--- src/Item.js
+++ src/Item.js
@@ -1,5 +1,13 @@
+// @flow
+
 export default class Item{
 	//an item has all attributes
 	//unless limited in mvp2
+	id:number;
+	parent:number;
+	data: Object;
+
+
+
 	
 }
---
+++ src/client/main.js
@@ -0,0 +1,9 @@
+// @flow
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+
+let div = document.getElementById('container')
+div.innerHTML = 'hello world'
+console.log('yolod');
