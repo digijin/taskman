b8230d7f8336b13f37c010d85f42bc32e7213fd7
digijin <james@digijin.com>
2017-04-03 21:16:19 +1000

    item add form

--- src/client/Item/Add.js
+++ src/client/Item/Add.js
@@ -4,33 +4,67 @@ import React from 'react'
 import { connect } from 'react-redux';
 
 import DropDown from '../component/DropDown'
+import type Type from 'Type';
+
+type Props = {
+	types: Array<Type>
+}
 
 class ItemAdd extends React.Component{
+
+	constructor(props){
+		super(props)
+		this.state = {}
+		this.submit = this.submit.bind(this)
+	}
 	render() {
 
-		let typeOptions = this.props.type.map(t => {
+		let typeOptions = this.props.types.map(t => {
 			return {value: t.getId(), label: t.getName()}
 		})
+
+		let fields = [];
+		if(this.state.type){
+			let type = this.props.types.filter(t => {return t.getId() == this.state.type})[0];
+			type.getDataFields().forEach(f => {
+
+				fields.push(<div key={f}>
+					{f}<br />
+					<input onChange={e => {
+						this.state[f] = e.target.value;
+						this.setState(this.state)
+						
+					}} type="text" placeholder={f} value={this.state[f]} />
+				</div>
+				)
+			})
+		}
 
 		return <div className="panel panel-default">
 			<div className="panel-heading">Add Item</div>
 			type: <DropDown 
-				onChange={(e) => {console.log(e);}} 
+				selected={this.state.type}
+				onChange={(e) => {this.setState({type:e});}} 
 				options={typeOptions} />
-			<input type="submit" />
+				{fields}
+			<input onClick={this.submit} type="submit" />
 		</div>
+	}
+	submit(){
+		// console.log('state');
+		this.props.add(this.state);
 	}
 }
 function mapStateToProps(state:Object, props:Object):Object {
 	return {
-		type: state.type
+		types: state.type
 	};
 }
 
 function mapDispatchToProps(dispatch:Function, props:Object):Object {
 	return {
-		close: () => {
-			// dispatch({type:'CLOSE_CONTEXT_MENU'});
+		add: (item) => {
+			dispatch({type:'ADD_ITEM', item: item});
 		}
 	};
 }
--- src/client/component/DropDown.js
+++ src/client/component/DropDown.js
@@ -4,11 +4,13 @@ export default class DropDown extends React.Component {
 	render() {
 
 		let options = this.props.options.map(o => {
-			return <li onClick={() => {this.props.onChange(o.value)}} key={o.value}><a href="#">{o.label}</a></li>
+			return <li 
+				onClick={() => {this.props.onChange(o.value)}} 
+				key={o.value}><a href="#">{o.label}</a></li>
 		})
 		return <div className="dropdown">
 			<button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-				Dropdown
+				{this.props.selected}
 				<span className="caret"></span>
 			</button>
 			<ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
