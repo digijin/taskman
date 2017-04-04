f492a3589eedb37e0a6a3155efb08604f5baa1f7
digi <james@digijin.com>
2017-04-05 01:57:13 +1000

    edit mode

--- src/client/Item/Edit.js
+++ src/client/Item/Edit.js
@@ -1,12 +1,53 @@
 
 import React from 'react';
+import { connect } from 'react-redux';
+import Form from './Form'
 
 class ItemEdit extends React.Component{
+	
+	constructor(props) {
+		super(props)
+		this.submit = this.submit.bind(this);
+		this.formChange = this.formChange.bind(this)
+
+		let id = props.id;
+		if(!id) id = props.match.params.id;
+
+		let item = this.props.items.filter(i=>{return i.getId() == id})[0];
+		this.state = item
+	}
+	formChange(e){
+		this.setState(e);
+	}
+	submit() {
+		this.props.submit(this.state);
+	}
 	render(){
-		return <div>
-			item edit
+		return <div className="panel panel-default">
+			<div className="panel-heading">Edit Item</div>
+			<div className="panel-body">
+				id: {this.state.id}
+				<Form onChange={this.formChange} state={this.state} />
+				<input onClick={this.submit} type="submit" />
+				<hr />
+				<pre>{JSON.stringify(this.state, null, 2)}</pre>
+			</div>
 		</div>
 	}
 }
 
-export default ItemEdit
+function mapStateToProps(state: Object, props: Object): Object {
+	return {
+		items: state.item
+	};
+}
+
+function mapDispatchToProps(dispatch: Function, props: Object): Object {
+	return {
+		submit: (item) => {
+			dispatch({ type: 'UPDATE_ITEM', item: item });
+		}
+	};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemEdit);
