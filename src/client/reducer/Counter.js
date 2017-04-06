baf5ef49a0d668ba3f13c5714c16a9a0b69425e9
digi <james@digijin.com>
2017-04-06 23:32:33 +1000

    loading counter state riught

--- src/client/reducer/Counter.js
+++ src/client/reducer/Counter.js
@@ -2,6 +2,10 @@
 
 export default function counterReducer(state=1, action){
     switch(action.type){
+		case 'LOAD':
+			if(action.data.counter)
+				state = action.data.counter
+		break;
         case 'ADD_ITEM':
             state = state + 1
             break;
