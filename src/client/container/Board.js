f91a3fcee26714bf916878065c3a6b072fa97e09
digijin <james@digijin.com>
2017-04-14 20:37:15 +1000

    make board scrollable

--- src/client/container/Board.js
+++ src/client/container/Board.js
@@ -26,7 +26,15 @@ class Board extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {filters:[]}
+		this.scrollSpeed = 0;
+		setInterval(this.doScroll, 10)
 	}
+	doScroll = () => {
+		this.board.scrollLeft += this.scrollSpeed;
+	}
+	scrollSpeed: number
+
+	board: HTMLDivElement
 
 	render(){
 
@@ -85,8 +93,7 @@ class Board extends React.Component{
 				iconElementRight={<Add style={{"margin-top": 6}} />}
 			/>
 			<Filters onChange={this.onFilterChange} type={typeData} />
-			<div style={{
-				border: '8px solid purple',
+			<div onMouseMove={this.onMouseMove} ref={b => {this.board = b}} style={{
 				overflowX: 'scroll',
 				overflowY: 'scroll',
 				//width: '100%',
@@ -97,6 +104,17 @@ class Board extends React.Component{
 				right: 0,
 				left: 0
 				}}>
+				{/*<div 
+				onMouseOver={this.onLeftOver} onMouseOut={this.onLeftOut}
+				style={{
+					border: '8px solid purple',
+					position: 'absolute',
+					left: 0,
+					width: 150,
+					top: 0,
+					bottom: 0,
+					//zIndex: 1000
+				}}></div>*/}
 				<div className="board">
 					{columns}
 				</div>
@@ -106,6 +124,27 @@ class Board extends React.Component{
 	onFilterChange = (e) => {
 		this.setState({filters:e})
 	}
+	onMouseMove = (e) => {
+		// console.log('e', e);
+		// console.log('e', e.screenX, e.pageX, e.clientX, window.innerWidth);
+		let size = 150;
+		this.scrollSpeed *=0.5;
+		if(e.clientX < size){
+			this.scrollSpeed --
+		}
+		if(e.clientX > (window.innerWidth - size)){
+			this.scrollSpeed ++
+		}
+		// debugger
+	}
+	// onLeftOver = (e) => {
+	// 	// console.log('left', this.board, this.board.scrollLeft)
+	// 	this.board.scrollLeft -= 10
+
+	// }
+	// onLeftOut = (e) => {
+	// 	// console.log('left out')
+	// }
 }
 
 function mapStateToProps(state: Object, props: Object): Object {
